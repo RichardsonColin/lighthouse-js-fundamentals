17738a299d8b9930640b4dcdf8197b98a10e28ce
Colin Richardson <richardsonjcolin@gmail.com>
2017-09-20 18:24:43 +0000

    Adds merge arrays exercise

---
+++ concat.js
@@ -0,0 +1,12 @@
+function merge(firstArray, secondArray) {
+  var counter = 0;
+
+  for(counter = 0; counter < secondArray.length; counter++) {
+    firstArray.push(secondArray[counter]);
+  }
+  return firstArray.sort();
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
